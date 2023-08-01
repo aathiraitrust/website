a853dc8cf0453155eb62bd24eb67454b502236f4
harishkannarao <harishkannarao@users.noreply.github.com>
2023-08-01 23:30:22 +0100

    Starting nav bar with Aathirai

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,25 @@
 
 // These styles apply to every route in the application
 import '../styles/scss/global.scss';
+import Link from 'next/link';
 
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body className="container">{children}</body>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="container">
+        <nav className="navbar bg-body-tertiary">
+          <div className="container">
+            <Link className="navbar-brand" href="/">
+              <img src="/img/logo/aathirai_logo.png" alt="Aathirai" width="30" height="24" />
+            </Link>
+          </div>
+        </nav>
+        {children}
+      </body>
+    </html>
+  )
+}
