4869e7f4f12d9e05c0757124ec08cd894fe4713e
harishkannarao <harishkannarao@users.noreply.github.com>
2023-08-18 23:05:08 +0100

    Changing log size

--- components/header/nav/NavigationBar.tsx
+++ components/header/nav/NavigationBar.tsx
@@ -3,13 +3,13 @@
 import Link from 'next/link';
 
 export function NavigationBar() {
-    return (
-        <nav className="navbar bg-body-tertiary">
-          <div className="container">
-            <Link className="navbar-brand" href="/">
-              <img data-testid="website-logo" src="/img/logo/aathirai_logo.png" alt="Aathirai" width="30" height="24" />
-            </Link>
-          </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar bg-body-tertiary">
+      <div className="container">
+        <Link className="navbar-brand" href="/">
+          <img data-testid="website-logo" src="/img/logo/aathirai_logo.png" alt="Aathirai" className='site-logo' />
+        </Link>
+      </div>
+    </nav>
+  );
 }
