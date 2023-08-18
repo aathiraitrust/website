e226665f6262b6161566446ed44b645f98e6637a
harishkannarao <harishkannarao@users.noreply.github.com>
2023-08-18 23:39:44 +0100

    Applying style to nav bar

--- components/header/nav/NavigationBar.tsx
+++ components/header/nav/NavigationBar.tsx
@@ -1,14 +1,30 @@
 'use client';
 
 import Link from 'next/link';
+import { useState } from 'react';
 
 export function NavigationBar() {
+  const [collapse, setCollapse] = useState(true);
+
   return (
-    <nav className="navbar bg-body-tertiary">
-      <div className="container">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <button className="navbar-toggler" type="button" onClick={() => setCollapse(!collapse)} aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <Link className="navbar-brand" href="/">
           <img data-testid="website-logo" src="/img/logo/aathirai_logo.png" alt="Aathirai" className='site-logo' />
         </Link>
+        <div className={collapse ? 'collapse navbar-collapse' : 'navbar-collapse'}>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">About Us</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
