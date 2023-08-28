203ad1e7e42e14af983661e1f0c4ca0d0c61b590
harishkannarao <harishkannarao@users.noreply.github.com>
2023-08-28 22:02:29 +0100

    Adding about-us page

---
+++ app/about-us/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function Page() {
+  return (
+    <div className="text-center">
+      <h2 className='text-primary'>Aathirai Educational And Charitable Trust</h2>
+    </div>
+  );
+}
