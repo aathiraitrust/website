bd1a0f1f43892c7b56fa1168a636b908b6e9a097
harishkannarao <harishkannarao@users.noreply.github.com>
2023-08-28 12:33:04 +0100

    Using activeKey

--- components/header/nav/NavigationBar.tsx
+++ components/header/nav/NavigationBar.tsx
@@ -15,18 +15,14 @@ export function NavigationBar() {
             <img data-testid="website-logo" src="/img/logo/aathirai_logo.png" alt="Aathirai" className='site-logo' />
           </Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="/" active>Home</Nav.Link>
-              <Nav.Link href="/">About Us</Nav.Link>
-              <Nav.Link href="/">Our Beneficiaries</Nav.Link>
-              <NavDropdown title="Events" id="basic-nav-dropdown">
-                <NavDropdown.Item href="/" active>Our Events</NavDropdown.Item>
-                <NavDropdown.Item href="/">
-                  Rising Star Projects
-                </NavDropdown.Item>
-              </NavDropdown>
-              <Nav.Link href="/">To Donate</Nav.Link>
-              <Nav.Link href="/">Contact Us</Nav.Link>
+            <Nav activeKey="/" className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about-us">About Us</Nav.Link>
+              <Nav.Link href="/our-beneficiaries">Our Beneficiaries</Nav.Link>
+              <Nav.Link href="/our-events">Our Events</Nav.Link>
+              <Nav.Link href="/rising-star-projects">Rising Star Projects</Nav.Link>
+              <Nav.Link href="/to-donate">To Donate</Nav.Link>
+              <Nav.Link href="/contact-us">Contact Us</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
