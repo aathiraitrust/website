8eea0b33dd7d2dc41dff1450b06b8a13d9312ca4
harishkannarao <harishkannarao@users.noreply.github.com>
2023-08-28 12:40:40 +0100

    Using path from nextjs

--- components/header/nav/NavigationBar.tsx
+++ components/header/nav/NavigationBar.tsx
@@ -3,9 +3,11 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { usePathname } from 'next/navigation';
 
 export function NavigationBar() {
+  const pathname = usePathname();
+
   return (
     <div>
       <Navbar expand="lg" sticky="top" className="bg-body-tertiary">
@@ -15,14 +17,14 @@ export function NavigationBar() {
             <img data-testid="website-logo" src="/img/logo/aathirai_logo.png" alt="Aathirai" className='site-logo' />
           </Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav activeKey="/" className="ms-auto">
+            <Nav activeKey={pathname} className="ms-auto">
               <Nav.Link href="/">Home</Nav.Link>
-              <Nav.Link href="/about-us">About Us</Nav.Link>
-              <Nav.Link href="/our-beneficiaries">Our Beneficiaries</Nav.Link>
-              <Nav.Link href="/our-events">Our Events</Nav.Link>
-              <Nav.Link href="/rising-star-projects">Rising Star Projects</Nav.Link>
-              <Nav.Link href="/to-donate">To Donate</Nav.Link>
-              <Nav.Link href="/contact-us">Contact Us</Nav.Link>
+              <Nav.Link href="/about-us/">About Us</Nav.Link>
+              <Nav.Link href="/our-beneficiaries/">Our Beneficiaries</Nav.Link>
+              <Nav.Link href="/our-events/">Our Events</Nav.Link>
+              <Nav.Link href="/rising-star-projects/">Rising Star Projects</Nav.Link>
+              <Nav.Link href="/to-donate/">To Donate</Nav.Link>
+              <Nav.Link href="/contact-us/">Contact Us</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
