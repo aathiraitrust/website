01e193f0299e327c9e46e5698150b41a55ad2489
harishkannarao <harishkannarao@users.noreply.github.com>
2023-08-31 22:59:47 +0100

    Adding content to about us page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -2,8 +2,44 @@
 
 export default function Page() {
   return (
-    <div className="text-center">
-      <h2 className='text-primary'>Aathirai Educational And Charitable Trust</h2>
-    </div>
+    <>
+      <div className="row">
+        <h3 className="text-primary text-center">About Us</h3>
+        <p>
+          The Aathirai Educational and Charitable Trust was founded by young professionals in 2009 with an objective to
+          promote literacy and higher education in the country. The Trust is registered in the name of “AATHIRAI EDUCATIONAL
+          AND CHARITABLE TRUST” at Anna Nagar Registrar Office under No.189/bk4/2010 on 05-March-2010.
+          We have obtained exemption under section 80G of Income Tax for all donations to our Trust.
+        </p>
+      </div>
+      <div className="row">
+        <h3 className="text-primary text-center">Who we are</h3>
+        <p><strong>History</strong></p>
+        <p>
+          “Life is short and vanities of this world are transient
+          <br />
+          But they alone LIVE WHO LIVE FOR OTHERS”
+          <br />
+          - Swami Vivekananda
+        </p>
+        <p>
+          Inspired by these words, a bunch of young professionals joined our hands to offer help to the needy
+          around us.
+        </p>
+        <p>
+          Our main aim is to inspire and motivate other professionals to take part in social activities and
+          collectively
+          grow into a tree offering fruits, shade and oxygen to those who are in need.
+        </p>
+        <p>
+          With the above aim and guided by experienced people, we envisioned a community for caregivers. The seed
+          was sown
+          on 12th December, 2009.
+        </p>
+        <p>
+        We named it "AATHIRAI". Aathirai is a character in the Tamil Epic Manimegalai composed in 6th Century CE symbolizing “Purity”. Manimegalai was composed by Seethalai Sathanar. It is a sequel to another great epic Silapathikaram. Manimekalai is the daughter of Kovalan and Madhavi. According to the poem, Maṇimekalai studies the six systems of philosophy of Hinduism and other prevalent religions of the time and compares them to the teachings of the Buddha. She is most impressed with Buddhism. She is bestowed with the magic never-failing begging bowl called Amiṛtha Surabhi (is something equivalent to the <strong>cornucopia</strong> (from Latin cornu copiae) or <strong>horn of plenty</strong> is a symbol of abundance and nourishment, commonly a large horn-shaped container overflowing with produce, flowers, nuts, other edibles, or wealth in some form) which will always provide food to alleviate hunger. But this Amirtha Surabhi will become a source of ever flowing food only when it is first filled by the most purest of the women. Hearing about Aathirai, Manimegalai received bikshai from Aathirai after which the Amirtha Surabhi overflows to alleviate poverty.
+        </p>
+      </div>
+    </>
   );
 }
