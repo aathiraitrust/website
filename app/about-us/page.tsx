6b90212e10518a1a5f8019bd6c1237514ff324fa
harishkannarao <harishkannarao@users.noreply.github.com>
2023-09-01 19:22:23 +0100

    Adding more content to about us page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -37,7 +37,24 @@ export default function Page() {
           on 12th December, 2009.
         </p>
         <p>
-        We named it "AATHIRAI". Aathirai is a character in the Tamil Epic Manimegalai composed in 6th Century CE symbolizing “Purity”. Manimegalai was composed by Seethalai Sathanar. It is a sequel to another great epic Silapathikaram. Manimekalai is the daughter of Kovalan and Madhavi. According to the poem, Maṇimekalai studies the six systems of philosophy of Hinduism and other prevalent religions of the time and compares them to the teachings of the Buddha. She is most impressed with Buddhism. She is bestowed with the magic never-failing begging bowl called Amiṛtha Surabhi (is something equivalent to the <strong>cornucopia</strong> (from Latin cornu copiae) or <strong>horn of plenty</strong> is a symbol of abundance and nourishment, commonly a large horn-shaped container overflowing with produce, flowers, nuts, other edibles, or wealth in some form) which will always provide food to alleviate hunger. But this Amirtha Surabhi will become a source of ever flowing food only when it is first filled by the most purest of the women. Hearing about Aathirai, Manimegalai received bikshai from Aathirai after which the Amirtha Surabhi overflows to alleviate poverty.
+          We named it "AATHIRAI". Aathirai is a character in the Tamil Epic Manimegalai composed in 6th Century CE symbolizing “Purity”. Manimegalai was composed by Seethalai Sathanar. It is a sequel to another great epic Silapathikaram. Manimekalai is the daughter of Kovalan and Madhavi. According to the poem, Maṇimekalai studies the six systems of philosophy of Hinduism and other prevalent religions of the time and compares them to the teachings of the Buddha. She is most impressed with Buddhism. She is bestowed with the magic never-failing begging bowl called Amiṛtha Surabhi (is something equivalent to the <strong>cornucopia</strong> (from Latin cornu copiae) or <strong>horn of plenty</strong> is a symbol of abundance and nourishment, commonly a large horn-shaped container overflowing with produce, flowers, nuts, other edibles, or wealth in some form) which will always provide food to alleviate hunger. But this Amirtha Surabhi will become a source of ever flowing food only when it is first filled by the most purest of the women. Hearing about Aathirai, Manimegalai received bikshai from Aathirai after which the Amirtha Surabhi overflows to alleviate poverty.
+        </p>
+      </div>
+      <div className="row">
+        <h3 className="text-primary text-center">What we do</h3>
+        <p><strong>Scholarships and Grants</strong></p>
+        <p>
+          Since its inception, the trust has promoted education mainly through scholarships and grants to deserving and needy students.
+          The scholarship has been set up to specifically empower students from the disadvantaged sections of our society. It is given to students belonging to lower income groups, who wish to pursue their higher education at recognized institutions.
+          Trust is keen in supporting students and encouraging them to be qualified to improve their standard of living, which will bring social and economic development.
+        </p>
+        <p><strong>Events</strong></p>
+        <p>
+        We have conducted various events successfully through our Trust, more about events can be found on the <a href="/our-events/">Events Page</a>
+        </p>
+        <p><strong>Rising Star Project</strong></p>
+        <p>
+        More about Rising Star Project can be found on the <a href="/rising-star-projects/">Rising Star Project</a>
         </p>
       </div>
     </>
