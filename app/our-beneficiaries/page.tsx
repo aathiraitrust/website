248c8df64dba9b464b460db2545cfe0dc36c40ff
harishkannarao <harishkannarao@users.noreply.github.com>
2023-09-06 21:32:45 +0100

    Updating our beneficiaries page

---
+++ app/our-beneficiaries/page.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+export default function Page() {
+  return (
+    <>
+      <div className="row">
+        <h3 className="text-primary text-center">Our Beneficiaries</h3>
+        <h4 className="text-primary text-center">Care Homes/Schools/Community</h4>
+        <ul>
+          <li>Families near Pattaravakkam, Karukku, Menampedu, Sidco Nagar - Villivakkam, Nammalwarpet, Chennai</li>
+          <li>Families near Erikkarai, Surakuppam, Agraharam- Srinivasapuram around Korattur, Chennai</li>
+          <li>Families near Ayanavaram, Chennai</li>
+          <li>Educational Assistance Day, Erikarai, Korattur, Chennai</li>
+          <li>Anganwadi Mayyam Government Nursery School, Kavarapettai</li>
+          <li>Residents of Magillan Kottai village near Pattukottai</li>
+          <li>Kalaiselvi Karunalaya Social Welfare Society, Mogappair West, Chennai</li>
+          <li>Government School, Villivakkam, Chennai</li>
+          <li>Government Primary School, Asanabudur Village</li>
+          <li>Government Middle School, Panapakkam Village</li>
+          <li>Government Primary School, Kavalpatti Village</li>
+          <li>Government Higher Secondary School, Medur, Ponneri Taluk, Thiruvallur District</li>
+          <li>Annai Therasa Social Welfare Association Trust, Dasukuppam Village, Uthukottai Post,Tamil Nadu</li>
+          <li>Bhagavan Sri Sathya Sai Nursery and Primary School, Nallatoor, Tiruvallur District</li>
+          <li>Truth foundation, Villangadu, Pakkam, Redhills, Chennai</li>
+          <li>Irular Colony, Nandhiyambakkam</li>
+          <li>Government School, Villivakkam, Chennai</li>
+          <li>Shelter Children Home, Kolathur, Chennai</li>
+          <li>Motto Children New Way, Ponneri</li>
+          <li>Saranalaya Children Home, Kovur</li>
+          <li>Mahalakshmi Foundation, Avadi, Chennai</li>
+          <li>HIV Affected Child Home, Korattur, Chennai</li>
+          <li>Samarpana, Kolathur, Chennai</li>
+          <li>Surabi Trust , Villivakkam, Chennai</li>
+          <li>Anbu Karangalin Anbu Illam, Ponneri</li>
+          <li>Annai Anbalaya Trust, Attipattu</li>
+          <li>Udhavum Ullangal Illam, Maraimalai Nagar & Adambakkam, Chennai</li>
+          <li>Annai Bharatha Matha Educational and Charitable Trust, Redhills, Chennai</li>
+          <li>Home Children's home, Pakkam</li>
+        </ul>
+        <h4 className="text-primary text-center">Individuals</h4>
+        <ul>
+          <li>A. Sreeman Pandi, Aalim Muhammed Salegh Polytechnic College, Nizara Educational Campus, Muthapudupet, Avadi, IAF, Chennai - 600 055</li>
+          <li>Ritesh, Egmore, Chennai</li>
+          <li>R.Kalimuthu, BA Economics (first year, second sem) in The American College, Madurai</li>
+          <li>K.Surya BBA degree course at Sree Sastha Arts & Science College, Chembarambakkam</li>
+          <li>Lourthu Sahaya Mary A, B.Ed., St.Xavier's College of Education, Palayamkottai</li>
+          <li>Dhivya Nawin Santh P S, Kanyakumari Government Medical College, Asaripallam</li>
+          <li>C. Monika, 2nd standard, Tahir Public School, Old Guddadahalli, Bangalore</li>
+          <li>C. Yuvashree, 5th standard, Tahir Public School, Old Guddadahalli, Bangalore</li>
+          <li>R. Bhuavaneshwari, B.Com., Bhakthavatsalam Memorial College for Women, Chennai</li>
+          <li>S. Ajith, B.Com., Meenakshi Ramasamy Arts and Science College, Thanthanur, Udayarpalayam, Ariyalur District, Tamil Nadu</li>
+          <li>V.Nithya, B.Sc Computer Science, Ponnusamy Nadar College of Arts and Science, Tiruvallur</li>
+          <li>B.Swathi, B.Com, Bakthavatchalam College for Women, Korattur, Chennai</li>
+          <li>A Monukshvi Aasi VI std, Sri Venkateshwara Matriculation Hr. Sec. School, Alwarpet, Chennai</li>
+          <li>A Vimosh Aasi IX std Sri Venkateshwara Matriculation Hr. Sec. School, Alwarpet, Chennai</li>
+          <li>V. Senthilkumar, B.Tech, Industrial Bio Technology, Government College of Technology, Coimbatore</li>
+          <li>N.Elakiya, BBA, SriRam College of Arts and Science, Thiruvallur District</li>
+          <li>V. Devadoss, B.Ed Maths, SRM University, Chennai</li>
+          <li>T.J. Srivatsa, VII STd, Unity Matriculation School, Ponneri</li>
+          <li>T.J. Sreelakshmi, IX Std, Unity Matriculation School, Ponneri</li>
+          <li>Nandhini, X Std, Soundarapandian Hr. Sec. School, Ayanavaram, Chennai</li>
+          <li>Shivaranjani, XII Std, Singaram Pillari Girls Hr. Sec. School, Villivakkam, Chennai</li>
+          <li>Vimal Jeba Raj, XI Std, Soundarapandian Hr. Sec. School, Ayanavaram, Chennai</li>
+        </ul>
+      </div>
+    </>
+  );
+}
