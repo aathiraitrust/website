336ff0346e94402ad9514e17f718d72b3919258c
harishkannarao <harishkannarao@users.noreply.github.com>
2023-09-19 13:27:56 +0100

    Collapsing navbar on selection

--- components/header/nav/NavigationBar.tsx
+++ components/header/nav/NavigationBar.tsx
@@ -12,7 +12,7 @@ export function NavigationBar() {
 
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary fixed-top container">
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary fixed-top container">
         <Container fluid>
           <Navbar.Brand href="/">
             <img data-testid="website-logo" src="/img/logo/aathirai_logo.png" alt="Aathirai" className='site-logo' />
@@ -20,13 +20,13 @@ export function NavigationBar() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav activeKey={pathname} className="ms-auto">
-              <Nav.Link as={Link} data-testid="home-link" href="/">Home</Nav.Link>
-              <Nav.Link as={Link} data-testid="about-us-link" href="/about-us/">About Us</Nav.Link>
-              <Nav.Link as={Link} data-testid="our-beneficiaries-link" href="/our-beneficiaries/">Our Beneficiaries</Nav.Link>
-              <Nav.Link as={Link} data-testid="our-events-link" href="/our-events/">Our Events</Nav.Link>
-              <Nav.Link as={Link} data-testid="rising-star-projects-link" href="/rising-star-projects/">Rising Star Projects</Nav.Link>
-              <Nav.Link as={Link} data-testid="to-donate-link" href="/to-donate/">To Donate</Nav.Link>
-              <Nav.Link as={Link} data-testid="contact-us-link" href="/contact-us/">Contact Us</Nav.Link>
+              <Nav.Link as={Link} passHref data-testid="home-link" href="/">Home</Nav.Link>
+              <Nav.Link as={Link} passHref data-testid="about-us-link" href="/about-us/">About Us</Nav.Link>
+              <Nav.Link as={Link} passHref data-testid="our-beneficiaries-link" href="/our-beneficiaries/">Our Beneficiaries</Nav.Link>
+              <Nav.Link as={Link} passHref data-testid="our-events-link" href="/our-events/">Our Events</Nav.Link>
+              <Nav.Link as={Link} passHref data-testid="rising-star-projects-link" href="/rising-star-projects/">Rising Star Projects</Nav.Link>
+              <Nav.Link as={Link} passHref data-testid="to-donate-link" href="/to-donate/">To Donate</Nav.Link>
+              <Nav.Link as={Link} passHref data-testid="contact-us-link" href="/contact-us/">Contact Us</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
