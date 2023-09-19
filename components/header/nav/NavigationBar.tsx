e40124ec4da70e83be4a9f3c82765623d572b5af
harishkannarao <harishkannarao@users.noreply.github.com>
2023-09-19 14:50:04 +0100

    Collapsing the navbar on clicking the logo

--- components/header/nav/NavigationBar.tsx
+++ components/header/nav/NavigationBar.tsx
@@ -5,16 +5,18 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import { useState } from 'react';
 
 export function NavigationBar() {
 
   const pathname = usePathname();
+  const [expanded, setExpanded] = useState(false);
 
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary fixed-top container">
+      <Navbar expanded={expanded} onToggle={(value) => setExpanded(value)} collapseOnSelect expand="lg" className="bg-body-tertiary fixed-top container">
         <Container fluid>
-          <Navbar.Brand href="/">
+          <Navbar.Brand href="/" as={Link} passHref onClick={(e) => setExpanded(false)}>
             <img data-testid="website-logo" src="/img/logo/aathirai_logo.png" alt="Aathirai" className='site-logo' />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
